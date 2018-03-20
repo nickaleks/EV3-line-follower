9cbe549fa85f26ebf8a2d736329f4cddf43eea3d
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-03-20 12:23:21 +0300

    Implement PID

--- driver.py
+++ driver.py
@@ -6,16 +6,26 @@ import time
 
 left = ev3.LargeMotor('outB')
 right = ev3.LargeMotor('outC')
-sensor = ev3.ColorSensor()
+left_sensor = ev3.ColorSensor('in2')
+right_sensor = ev3.ColorSensor('in3')
 button = ev3.Button()
 screen = ev3.Screen()
 
-setpoint = 16
-Kp = 7
+# light_setpoint = 0
+left_speed = 200
+right_speed = 200
+Kp = 30.0
+Kd = 10.0
+Ki = 0.4
+last_error = 0
+ivalue = 0
 
-def adjust_speed(left_speed, right_speed, sensor):
-    error = setpoint - sensor
-    speed_change = Kp * error
+def adjust_speed(left_speed, right_speed, error):
+    global last_error, ivalue
+    ivalue += error
+    speed_change = (Kp * error + (Ki * ivalue) + Kd * (error - last_error)) #* (left_speed / 200)
+    print("error: {}, derivative: {}, integral: {}".format(e, Kd * (error - last_error),  Ki * ivalue))
+    last_error = error
     return left_speed - speed_change, right_speed + speed_change
 
 
@@ -24,17 +34,20 @@ def stop():
     right.stop()
     exit()
 
+# if more than 0, going right from line, otherwise left
+def error(left_sensor, right_sensor):
+    return left_sensor - right_sensor
+
 if __name__ == "__main__":
-    left_speed = 200
-    right_speed = 200
     try:
         while(True):
-            light = sensor.ambient_light_intensity
-            new_left, new_right = adjust_speed(left_speed, right_speed, light)
-            print("sensor: {}, new_left: {}, new_right: {}".format(light, new_left, new_right))
+            left_light = left_sensor.ambient_light_intensity
+            right_light = right_sensor.ambient_light_intensity
+            e = error(left_light, right_light)
+            new_left, new_right = adjust_speed(left_speed, right_speed, e)
             left.run_forever(speed_sp=new_left)
             right.run_forever(speed_sp=new_right)
-            time.sleep(0.2)
+            # time.sleep(0.015)
     finally:
         left.stop()
         right.stop()
